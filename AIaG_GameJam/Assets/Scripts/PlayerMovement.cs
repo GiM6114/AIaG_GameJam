b5b1bec45c2b264d6977a28db17fcc2979a7a7ca
Oroshibu <oroshibu32@gmail.com>
2021-05-31 13:24:37 +0200

    player prefab + animations

--- AIaG_GameJam/Assets/Scripts/PlayerMovement.cs
+++ AIaG_GameJam/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed;
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
         
     public void Move(InputAction.CallbackContext ctx)
@@ -29,6 +31,23 @@ public class PlayerMovement : MonoBehaviour
         else if (movement.x <= -0.05f)
         {
             transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    private void Update()
+    {
+        if (Mathf.Abs(rb.velocity.magnitude) >= 0.05f)
+        {
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("player_walk"))
+            {
+                animator.Play("player_walk");
+            }
+        } else
+        {
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("player_idle"))
+            {
+                animator.Play("player_idle");
+            }
         }
     }
 }
