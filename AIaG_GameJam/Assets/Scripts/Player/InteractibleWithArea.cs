8dfb33a1c5feade437100069cf7609382084c7a6
GiM6114 <73024037+GiM6114@users.noreply.github.com>
2021-05-31 14:17:42 +0200

    ajout event

--- AIaG_GameJam/Assets/Scripts/Player/InteractibleWithArea.cs
+++ AIaG_GameJam/Assets/Scripts/Player/InteractibleWithArea.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class InteractibleWithArea : Interactible
 {
+    [SerializeField] string nameOfForbidden;
+
+    [NonSerialized] public Action interacted;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.name == nameOfForbidden)
         {
             Interacted();
+            interacted?.Invoke();
             if (!WorldEngine.i.HasRuleBeenBroke(lawIndex))
             {
                 WorldEngine.i.BreakRuleSign(lawIndex);
